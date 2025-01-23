4a560d0e6b9807516b8325a4aefdc74a9ff2f529
TestLabQA <qatestlab555@gmail.com>
2025-01-23 12:45:25 +0530

    checking and testng.xml file

--- src/test/java/com/LAMBDATEST_Playground/Checking.java
+++ src/test/java/com/LAMBDATEST_Playground/Checking.java
@@ -12,7 +12,9 @@ public class Checking {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.getProperties().list(System.out);
+		//System.getProperties().list(System.out);
+		
+		System.out.println("Hello");
 		
 
 	}
